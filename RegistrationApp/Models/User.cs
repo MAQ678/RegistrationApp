e4f9e94810d5a800fdd490a8cff4367b7cad7584
Md Abdul Quaum <md.abdul@samsung.com>
2025-05-21 21:29:29 +0600

    Add project files.

---
+++ RegistrationApp/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationApp.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+    }
+}
